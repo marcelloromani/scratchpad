4bbac83d5bacdd29ad5b2c300b92f7b176b398fa
Marcello Romani <marcello.romani@magentys.io>
2017-02-10 16:13:35 +0000

    mention optional option -d to daemonise the grid

--- selenium_workshop/test_selenium_grid_firefox.sh
+++ selenium_workshop/test_selenium_grid_firefox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "make sure you ran docker-compose up before running this test"
+echo "make sure you ran docker-compose up [-d] before running this test"
 
 pytest --driver Remote --capability browserName firefox test_pytest_selenium.py
