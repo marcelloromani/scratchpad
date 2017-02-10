b53af3b1f70b162b66c756867cd2836bcfae8bd6
Marcello Romani <marcello.romani@magentys.io>
2017-02-10 16:15:46 +0000

    mention URL of selenium grid console.

--- selenium_workshop/test_selenium_grid_firefox.sh
+++ selenium_workshop/test_selenium_grid_firefox.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "make sure you ran docker-compose up [-d] before running this test"
+echo "Access the selenium grid console at http://localhost:4444/grid/console"
 
 pytest --driver Remote --capability browserName firefox test_pytest_selenium.py
