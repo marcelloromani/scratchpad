c0470e69d4bf64ae616893d6dfb2cbcc4715e2fb
Marcello Romani <marcello.romani.enit@gmail.com>
2017-04-12 22:35:56 +0100

    Added new version based on centos 6

---
+++ dockerfiles/centos6base/run_ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SSH_PORT=2222
+
+#IMAGE_NAME=marcelloromani1975/centosbase:6.8
+IMAGE_NAME=centosbase:6.8
+
+echo "Connecto the host with:"
+echo "ssh -p $SSH_PORT admin@localhost"
+echo "Password: admin"
+echo
+echo "To connect with key-based auth:"
+echo "ssh -i `pwd`/files/id_rsa -p $SSH_PORT admin@localhost"
+
+docker run --rm -it -p $SSH_PORT:22 $IMAGE_NAME /bin/sh -c 'sudo service ssh start && /bin/bash'
