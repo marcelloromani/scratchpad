8ce8fa3fbd23c52e7203ebb5f93f11e51ec7596e
marcelloromani <marcello.romani@smartpension.co.uk>
2021-11-11 13:55:52 +0000

    formatting

--- script_skel.py
+++ script_skel.py
@@ -4,23 +4,27 @@
 Script skeleton.
 """
 
-import sys
 import logging
+import sys
 from argparse import ArgumentParser
 
 
 def optSetup():
     parser = ArgumentParser()
 
-    parser.add_argument("file",
-            type=str,
-            help="File to process")
+    parser.add_argument(
+        "file",
+        type=str,
+        help="File to process"
+    )
 
-    parser.add_argument("--log-level",
-            type=str,
-            choices=["DEBUG", "INFO", "ERROR"],
-            default="INFO",
-            help="Set log level")
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        choices=["DEBUG", "INFO", "ERROR"],
+        default="INFO",
+        help="Set log level"
+    )
 
     return parser
 
@@ -40,7 +44,7 @@ def main():
     args = parser.parse_args()
     logSetup(logging.getLevelName(args.log_level))
 
-    logger=logging.getLogger("main")
+    logger = logging.getLogger("main")
     logger.info("argument: {0}".format(args.file))
 
 
