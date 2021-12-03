9acd296dbd2bd159d539b953804df83bda335900
marcelloromani <marcello.romani@smartpension.co.uk>
2021-12-03 01:28:53 +0000

    fix main, remove redundant var

--- aoc-2021/day1/main.py
+++ aoc-2021/day1/main.py
@@ -4,16 +4,19 @@ from sys import stdin
 def count_increases(measurements: list) -> int:
     if len(measurements) <= 1:
         return 0
-    prev_meas = measurements[0]
     increase_count = 0
     for i in range(1, len(measurements)):
-        if measurements[i] > prev_meas:
+        if measurements[i] > measurements[i - 1]:
             increase_count += 1
-        prev_meas = measurements[i]
     return increase_count
 
 
-m = stdin.readlines()
-print(len(m))
-c = count_increases(m)
-print(c)
+def main():
+    m = stdin.readlines()
+    print(len(m))
+    c = count_increases(m)
+    print(c)
+
+
+if __name__ == "__main__":
+    main()
