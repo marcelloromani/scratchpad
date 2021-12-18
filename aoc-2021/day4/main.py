5e6342c9c1d975c4dde61a6674c0f871e6309fe9
marcelloromani <marcello.romani@smartpension.co.uk>
2021-12-18 15:40:50 +0100

    day4 - WIP

---
+++ aoc-2021/day4/main.py
@@ -0,0 +1,101 @@
+from sys import stdin
+
+
+def purge_blanks(data: list[str]):
+    for d in data:
+        d = d.strip()
+        if len(d) == 0:
+            continue
+        yield d
+
+
+def parse_board(board_text: str) -> list[list[int]]:
+    b = []
+    for line in board_text.split("\n"):
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        b.append([int(x) for x in line.split()])
+    return b
+
+
+def read_input(filename: str) -> (list[int], list[list[list[int]]]):
+    WAITING_FOR_NUMBERS: int = 1
+    WAITING_FOR_BOARDS: int = 2
+    READING_BOARD: int = 3
+
+    state = WAITING_FOR_NUMBERS
+
+    numbers = []
+    boards = []
+    board_txt = ""
+
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if state == WAITING_FOR_NUMBERS:
+                if len(line) == 0:
+                    continue
+                else:
+                    numbers = [int(x) for x in line.split(",")]
+                    state = WAITING_FOR_BOARDS
+            elif state == WAITING_FOR_BOARDS:
+                if len(line) == 0:
+                    continue
+                else:
+                    state = READING_BOARD
+                    board_txt = line
+            elif state == READING_BOARD:
+                if len(line) == 0:
+                    # finished reading a board
+                    board = parse_board(board_txt)
+                    boards.append(board)
+                    state = WAITING_FOR_BOARDS
+                else:
+                    # still reading the board
+                    board_txt += line
+            else:
+                raise ValueError("Unknown state")
+
+    return numbers, boards
+
+
+def num_pos_in_board(board: list[list[int]], n: int) -> (int, int):
+    row_count = len(board)
+    col_count = len(board[0])
+    for i in range(row_count):
+        for j in range(col_count):
+            if board[i][j] == n:
+                return i, j
+    return -1, -1
+
+
+def is_winning_board(board: list[list[int]], numbers: list[int]) -> bool:
+    row_count = len(board)
+    col_count = len(board[0])
+    # these array count how many times each row is found having a matching number
+    row_match_count = [0 for _ in range(row_count)]
+    col_match_count = [0 for _ in range(col_count)]
+    # look for all numbers in the board
+    for n in numbers:
+        row, col = num_pos_in_board(board, n)
+        if row != -1 and col != -1:
+            row_match_count[row] += 1
+            col_match_count[col] += 1
+    for r in row_match_count:
+        if r == col_count:
+            return True
+    for c in col_match_count:
+        if c == row_count:
+            return True
+    return False
+
+
+def main():
+    lines = stdin.readlines()
+
+    lines = list(purge_blanks(lines))
+
+
+if __name__ == "__main__":
+    main()
---
+++ aoc-2021/day4/tests/test_bingo.py
@@ -0,0 +1,110 @@
+import unittest
+
+from main import num_pos_in_board, is_winning_board, parse_board, read_input
+
+
+class TestBoard(unittest.TestCase):
+
+    def test_get_num_position(self):
+        board = [
+            [1, 2],
+            [3, 4],
+        ]
+        test_cases = [
+            {
+                "n": 1,
+                "pos": (0, 0),
+            },
+            {
+                "n": 2,
+                "pos": (0, 1),
+            },
+            {
+                "n": 3,
+                "pos": (1, 0),
+            },
+            {
+                "n": 4,
+                "pos": (1, 1),
+            },
+            {
+                "n": 5,
+                "pos": (-1, -1),
+            },
+        ]
+        for t in test_cases:
+            self.assertEqual(t["pos"], num_pos_in_board(board, t["n"]), t["n"])
+
+    def test_is_board_winning(self):
+        board = [
+            [1, 2],
+            [3, 4],
+        ]
+        test_cases = [
+            {
+                "nums": [1, 2],
+                "win": True,
+            },
+            {
+                "nums": [3, 4],
+                "win": True,
+            },
+            {
+                "nums": [1, 3],
+                "win": True,
+            },
+            {
+                "nums": [2, 4],
+                "win": True,
+            },
+            {
+                "nums": [1, 4],
+                "win": False,
+            },
+            {
+                "nums": [2, 3],
+                "win": False,
+            },
+        ]
+        for t in test_cases:
+            self.assertEqual(t["win"], is_winning_board(board, t["nums"]), t["nums"])
+
+
+class TestInput(unittest.TestCase):
+
+    def test_parse_board(self):
+        board_txt = """
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19
+        """
+        board = [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19],
+        ]
+        self.assertListEqual(board, parse_board(board_txt))
+
+    def test_read_input(self):
+        file = "input_00.txt"
+        numbers = [7, 4, 9]
+        boards = [
+            [
+                [22, 13, 17, 11, 0],
+                [8, 2, 23, 4, 24],
+                [21, 9, 14, 16, 7],
+                [6, 10, 3, 18, 5],
+                [1, 12, 20, 15, 19],
+            ]
+        ]
+        n, b = read_input(file)
+        self.assertListEqual(numbers, n)
+        self.assertListEqual(boards, b)
+
+
+if __name__ == '__main__':
+    unittest.main()
