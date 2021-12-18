1e88fc2a82005b8774772d18fb3c4e3b5245d771
Marcello Romani <marcello.romani.enit@gmail.com>
2021-12-18 15:37:32 +0000

    More tests.

--- aoc-2021/day4/tests/test_bingo.py
+++ aoc-2021/day4/tests/test_bingo.py
@@ -121,6 +121,29 @@ class TestInput(unittest.TestCase):
         self.assertListEqual(numbers, n)
         self.assertListEqual(boards, b)
 
+    def test_read_input_02(self):
+        file = "input_02.txt"
+        numbers = [7, 4, 9]
+        boards = [
+            [
+                [22, 13, 17, 11, 0],
+                [8, 2, 23, 4, 24],
+                [21, 9, 14, 16, 7],
+                [6, 10, 3, 18, 5],
+                [1, 12, 20, 15, 19],
+            ],
+            [
+                [3, 15, 0, 2, 22],
+                [9, 18, 13, 17, 5],
+                [19, 8, 7, 25, 23],
+                [20, 11, 10, 24, 4],
+                [14, 21, 16, 12, 6],
+            ],
+        ]
+        n, b = read_input(file)
+        self.assertListEqual(numbers, n)
+        self.assertListEqual(boards, b)
+
     def test_read_input_03(self):
         file = "input_03.txt"
         numbers = [7, 4, 9]
